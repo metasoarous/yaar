a61750998ed29849f5032707bff518bfedd3b207
Christopher Small <metasoarous@gmail.com>
2012-11-25 17:53:33 -0800

    added light sensor with serial logging

--- robot.ino
+++ robot.ino
@@ -13,6 +13,8 @@ int speedPin=9;
 
 int servoControlPin = 53;
 
+int lightSensorPin = 7;
+
 // Initialize servo and drive motor
 Servo turner;
 DriveMotor driver;
@@ -23,15 +25,24 @@ void turn(int degrees) {
   turner.write(90 - degrees);
 }
 
+int lightLevel()
+{
+  return analogRead(lightSensorPin);
+}
+
 
 // Cue arduino madness
 
 void setup() {
   driver.connect(pinI1, pinI2, speedPin);
   turner.attach(53);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
 }
 
 void loop() {
+  int ll = lightLevel();
+  Serial.print("Light Level is: ");
+  Serial.println(ll);
   turn(0);
   delay(1000);
   driver.forward(100);
